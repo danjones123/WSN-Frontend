8029a2dd5bc0f53c17fd0a67a2ca343da6cddc15
danjones123 <danielj.798@gmail.com>
2024-05-15 13:14:49 +0100

    Added embed link for youtube video to homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,23 @@ import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/GlobalHeader";
 import GroupPhoto from "../../assets/WSN Group photo - Copy.jpg";
-import { Typography, CardMedia, Box, Paper } from "@mui/material";
+import {
+  Typography,
+  CardMedia,
+  Box,
+  Paper,
+  useMediaQuery,
+} from "@mui/material";
 
 import MiddleBackgroundStrip from "./MiddleBackgroundStrip";
 import HomeOurAim from "./HomeOurAim";
 
 export default function Home() {
   const nav = useNavigate();
+  const isSmallScreen = useMediaQuery("(max-width: 600px)");
+  // const isMediumScreen = useMediaQuery(
+  //   "(min-width: 601px) and (max-width: 1000px)"
+  // );
 
   return (
     <Box>
@@ -24,6 +34,16 @@ export default function Home() {
         </Box>
         <MiddleBackgroundStrip />
         <HomeOurAim />
+        <iframe
+          width={isSmallScreen ? "320" : "560"}
+          height={isSmallScreen ? "180" : "315"}
+          src="https://www.youtube.com/embed/jNQXAC9IVRw?si=W749W8CZK_5IkNBX"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
       </center>
     </Box>
   );
